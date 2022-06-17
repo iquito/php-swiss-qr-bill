2cf44670f4b8fa677e9199e15370bae764e59971
Andreas <andreas.leathley@panaxis.ch>
2022-06-17 14:57:04 +0200

    Allow to get QR code image without creating a file

--- src/QrCode/QrCode.php
+++ src/QrCode/QrCode.php
@@ -66,6 +66,13 @@ final class QrCode
     {
         return $this->getQrCodeResult()->getDataUri();
     }
+    
+    /** @param "svg"|"png" $format */
+    public function getImageAsString(string $format): string
+    {
+        $this->setWriterByExtension($format);
+        return $this->getQrCodeResult()->getString();
+    }
 
     public function getText(): string
     {
